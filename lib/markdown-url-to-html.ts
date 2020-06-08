8880e9f26264601003b14d4fe3272bef0ae322c0
Nikolay Skinteev <distortriver@gmail.com>
2020-06-09 01:07:37 +0300

    add md extension to local links without it

--- lib/markdown-url-to-html.ts
+++ lib/markdown-url-to-html.ts
@@ -1,5 +1,8 @@
 import mdR from "./markdown-regex";
 
 export default function mdUrl(file: string) {
+  if(file.lastIndexOf(".") < 1){
+    file = `${file}.md`;
+  }
   return file.replace(mdR, ".html");
 }
